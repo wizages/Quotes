c95da9b73ffe1c617d732a960878b96592d961e6
Sam <wizagesmax@gmail.com>
2015-07-08 21:56:45 -0500

    Version 2
    Updated a bunch of things... check log on website for info.

--- Scripts/quote.js
+++ Scripts/quote.js
@@ -1,29 +1,37 @@
 var quotenumber = 0;
 var timeoutId = null;
 var feed;
+var result;
 
 function initialize() {
+    if (sourceFeed == "custom")
+    {
+        sourceFeed = customUrl;
+    }
     feed = new google.feeds.Feed(sourceFeed);
-        feed.load(function(result) {
-        if (!result.error) {
-            var container = document.getElementById("feed");
-            if (quotenumber == result.feed.entries.length) {
-                quotenumber = 0;
-            }
-            var entry = result.feed.entries[quotenumber];
-            container.style.color = fontColor;
-            container.style.fontSize = fontSize + "px";
-            container.style.fontFamily = fontName;
-            container.innerHTML = "<blockquote>" + entry.content + "<cite>" + entry.title +"</cite>" + "</blockquote>";
-            quotenumber++;
-        }
-    });
+    feed.setNumEntries(100);
     if (timeoutId != null) {
         clearTimeout(timeoutId);
     }
-    timeoutId = setTimeout(function() {
-        initialize();
+    feed.load(loopQuote);
+ }
 
-    }, (interval_time*3600000));
-
+function loopQuote(result) {
+    if (!result.error) {
+        var container = document.getElementById("feed");
+        var entry = result.feed.entries[quotenumber];
+        container.style.color = fontColor;
+        container.style.fontSize = fontSize + "px";
+        container.style.fontFamily = fontName;
+        container.innerHTML = "<blockquote>" + entry.content + "<cite>" + entry.title + "</cite>" + "</blockquote>";
+        quotenumber++;
+        timeoutId = setTimeout(function() {
+            if (quotenumber == result.feed.entries.length) {
+                quotenumber = 0;
+                initialize();
+            } else {
+                loopQuote(result);
+            }
+        }, (interval_time * 3600000));
+    }
 }
